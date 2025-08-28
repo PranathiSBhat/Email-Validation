accb7319f08dcc328c7ed06c0d43ef477389a189
PranathiSBhat <136635131+PranathiSBhat@users.noreply.github.com>
2025-08-28 16:27:43 +0530

    Update app.py
    database done

--- app.py
+++ app.py
@@ -1,10 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the Flask App!"
+def db_connection():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="root",
+        database="capstone_project"
+    )
+    return conn
 
-if __name__ == '__main__':
+@app.route("/", methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == "POST":
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = db_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        # fetch user by email and password
+        cursor.execute("SELECT * FROM user_sign WHERE email=%s AND password=%s", (email, password))
+        user = cursor.fetchone()
+
+        if user:
+            return redirect(url_for("landing"))
+        else:
+            error = "Invalid email or password!"
+
+    return render_template("login.html",error=error)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        # collecting the data
+        name = request.form["name"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        conn = db_connection()
+        cursor = conn.cursor()
+
+        try:
+            cursor.execute(
+                "INSERT INTO user_sign (name, email, password) VALUES (%s, %s, %s)",
+                (name, email, password),
+            )
+            conn.commit()
+        except mysql.connector.Error as err:
+            return f"Error: {err}"
+        finally:
+            cursor.close()
+            conn.close()
+
+        return redirect(url_for("login"))
+
+    return render_template("signup.html")
+
+@app.route("/landing")
+def landing():
+    return render_template("landing.html")
+
+if __name__ == "__main__":
     app.run(debug=True)
