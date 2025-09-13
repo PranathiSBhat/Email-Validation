a28548478a0580a6fa0b85d9eb303f9c169a08ac
Pranathi <pranathisbhat09@gmail.com>
2025-09-14 00:09:57 +0530

    integrated the dashboard part

--- app.py
+++ app.py
@@ -80,22 +80,7 @@ def signup():
 def landing():
     return render_template('landing.html')
 
-# #spam detection
-# @app.route("/spam", methods=["GET", "POST"])
-# def spam():
-#     classification_result = None
-#     email_content = ""
 
-#     if request.method == "POST":
-#         email_content = request.form.get("message", "").strip()
-
-#         if email_content:
-#             email_vector = tfidf_vectorizer.transform([email_content])
-#             prediction = spam_model.predict(email_vector)[0]
-#             classification_result = "Spam" if prediction == 1 else "Ham"
-
-
-#     return render_template("spam.html", result=classification_result, email=email_content)
 
 @app.route("/spam", methods=["GET", "POST"])
 def spam():
@@ -147,6 +132,14 @@ def validations():
             report = {"total": total, "valid": valid, "invalid": invalid}
 
     return render_template("validation.html", result=result, report=report)
+
+@app.route("/spam_dashboard")
+def spam_dashboard():
+    return render_template("spam_dashboard.html") # need to  change according to file name 
+
+@app.route("/validation_dashboard")
+def validation_dashboard():
+    return render_template("validation_dashboard.html") # need to change according to file name
 
 
 if __name__ == "__main__":
